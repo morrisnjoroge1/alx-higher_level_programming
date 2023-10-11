fb62bce0808a4ff6bebe6ba854ec63e0e258f11f
morris <mnjoroge25439@gmail.com>
2023-10-11 12:24:14 -0700

    data:structure

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+
+    # Create a new dictionary to store the results
+    new_dictionary = {}
+    for key, value in a_dictionary.items():
+        new_dictionary.update({key: (value * 2)})
+    return new_dictionary

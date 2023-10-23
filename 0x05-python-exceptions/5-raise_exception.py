3407c16500fcc22d54ea89b62bbeefdfc5680a19
morris <mnjoroge25439@gmail.com>
2023-10-23 12:29:34 -0700

    expectations

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,3 @@
+def raise_exception():
+  """Raises a type exception."""
+  raise TypeError
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+def raise_exception_msg(message=""):
+  """Raises a name exception with a message.
+
+  Args:
+    message: The message to include in the exception.
+  """
+  raise NameError(message)
+

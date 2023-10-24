0eb4e8fd9a28cb8f46e4e4c2babcd033ab1a8d57
morris <mnjoroge25439@gmail.com>
2023-10-23 21:31:11 -0700

    expectations

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -30,3 +30,53 @@ void print_python_list(PyObject *p)
             print_python_float(item);
     }
 }
+
+/**
+ * print_python_bytes - gives data of the PyBytesObject
+ * @p: the PyObject
+ */
+void print_python_bytes(PyObject *p)
+{
+    Py_ssize_t a, size;
+    char *str;
+    size = ((PyVarObject *)p)->ob_size;
+
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
+
+    if (size > 10)
+        size = 10;
+
+    str = ((PyBytesObject *)p)->ob_sval;
+    printf("  first %ld bytes: ", size);
+    for (a = 0; a < size; a++)
+    {
+        printf("%02hhx", str[a]);
+        if (a < size - 1)
+            printf(" ");
+    }
+    printf("\n");
+}
+
+/**
+ * print_python_float - gives data of the PyFloatObject
+ * @p: the PyObject
+ */
+void print_python_float(PyObject *p)
+{
+	double value = 0;
+	char *string = NULL;
+
+	fflush(stdout);
+	printf("[.] float object info\n");
+
+	if (!PyFloat_CheckExact(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	value = ((PyFloatObject *)p)->ob_fval;
+	string = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	printf("  value: %s\n", string);
+}

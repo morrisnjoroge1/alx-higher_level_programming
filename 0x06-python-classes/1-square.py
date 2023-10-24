456b731edd7583b7b23a59b5fc16b68e66f3fa51
morris <mnjoroge25439@gmail.com>
2023-10-24 10:28:33 -0700

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+class that defines a square
+'''
+
+
+class square:
+def __init__(self, size):
+'''
+new square
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+'''
+
+self.__size = size

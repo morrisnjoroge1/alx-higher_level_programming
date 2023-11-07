8d4a7e03540719186ccdf2e03054cbde6c42e1fe
morris <mnjoroge25439@gmail.com>
2023-11-07 10:24:29 -0800

    input

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a string-to-JSON function"""
+"""
+
+This module defines a string-to-JSON function
+
+"""
+
 import json
 
 
 def to_json_string(my_obj):
-  """Returns the JSON representation of an object (string).
+    """Return the JSON representation of a string object.
 
-  Args:
-    my_obj: The object to serialize.
+    Args:
+        my_obj: The object to serialize.
+    """
 
-  Returns:
-    The JSON representation of the object (string).
-  """
-
-  def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
     return json.dumps(my_obj)

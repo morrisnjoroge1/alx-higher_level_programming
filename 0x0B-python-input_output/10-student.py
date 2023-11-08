a2c411fe6f1c740e93c73c9ae3a224517f870c4b
morris <mnjoroge25439@gmail.com>
2023-11-08 10:17:48 -0800

    output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
-"""
-
-class Student that defines a student.
-
-"""
+"""Defines a class Student."""
 
 
 class Student:
-    """represents students"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """initialize new student.
+        """Initialize a new Student.
 
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
 
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
         Args:
-            retrieves a dictionary representation of a Student
-            instance (same as 8-class_to_json.py):
-            
-            If attrs is a list of strings, only attribute names
-            contained in this list must be retrieved
-
-            Otherwise, all attributes must be retrieved
+            attrs (list): (Optional) The attributes to represent.
         """
-
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}

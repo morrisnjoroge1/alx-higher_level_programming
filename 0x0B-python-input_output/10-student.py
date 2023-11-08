2b5a573f19320e0d15f35e51c08ed89d01152d54
morris <mnjoroge25439@gmail.com>
2023-11-08 10:22:39 -0800

    input

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+
+class Student that defines a student.
+
+"""
 
 
 class Student:
-    """Represent a student."""
+    """represents students"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """initialize new student.
 
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age

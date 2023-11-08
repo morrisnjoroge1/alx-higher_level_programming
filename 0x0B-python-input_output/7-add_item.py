116bc6ba05c32deb516b9aa435e403277e39d615
morris <mnjoroge25439@gmail.com>
2023-11-07 22:20:19 -0800

    input

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
+"""This module adds all arguments to a Python list and save them to a file."""
 
-script that adds all arguments to a Python list, and then save them to a file
-
-"""
 
 import sys
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file

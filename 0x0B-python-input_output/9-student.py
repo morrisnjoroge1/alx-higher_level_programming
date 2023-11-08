a79dad86a847ff6116dfff87bf13b30595903e90
morris <mnjoroge25439@gmail.com>
2023-11-07 22:40:20 -0800

    inh

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+
+class Student that defines a student.
+
+"""
+
+
+class Student
+""" reps student"""
+
+def __init__(self, first_name, last_name, age):
+    """ initialize student.
+
+    Args:
+         first_name (str): The first name of the student.
+         last_name (str): The last name of the student.
+          age (int): The age of the student.
+    """
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
+        return self.__dict__

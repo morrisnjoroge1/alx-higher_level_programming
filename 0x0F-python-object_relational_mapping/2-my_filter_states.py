7c80b18afcadc7cf0c63095b3fd5e97566e1badc
morris <mnjoroge25439@gmail.com>
2024-01-17 19:28:42 -0800

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,42 @@
 #!/usr/bin/python3
 """
-displays all values in the states table of hbtn_0e_0_usa where
-name matches the argument."""
+Module that connects a python script to a database
+"""
 
 if __name__ == "__main__":
 
     import MySQLdb
-    import sys
+    from sys import argv
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-            )
+    # Connect database using command-line arguments
+    my_db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+        )
 
-    conn = db.cursor()
-    conn.execute(
-            "SELECT * FROM states WHERE name = '{}' ORDER BY state.id"
-            .format(sys.argv[4]))
-    rows = conn.fetchall()
-    for row in rows:
+    # Create cursor obj to interact with database
+    my_cursor = my_db.cursor()
+
+    # Execute a SELECT query to fetch data
+    my_cursor.execute(
+        """
+        SELECT * FROM states  WHERE name LIKE BINARY '{}'
+        ORDER BY states.id ASC
+        """.format(argv[4])
+        )
+
+    # fetch all the data returned by the query
+    my_data = my_cursor.fetchall()
+
+    # Iterate through the fetched data and print each row
+    for row in my_data:
         print(row)
 
-    conn.close()
-    db.close()
+    # Close all cursors
+    my_cursor.close()
+
+    # Close all databases
+    my_db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,26 +1,38 @@
 #!/usr/bin/python3
 """
-displays all values in the states table of hbtn_0e_0_usa where
-name matches the argument having a safe from MySQL injections!."""
+Module that connects a python script to a database
+"""
 
-if __name__ == "__main__"
-import sys
-import MySQLdb
+if __name__ == "__main__":
 
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
-            )
+    import MySQLdb
+    from sys import argv
 
-    conn = db.cursor()
-    conn.execute(
-            "SELECT * FROM states WHERE name = %s ORDER BY state.id",
-            (sys.argv[4]))
-    rows = conn.fetchall()
-    for row in rows:
-        print(row)                                                                  
-    conn.close()
-    db.close()
+    # Connect database using command-line arguments
+    my_db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+        )
+
+    # Create cursor obj to interact with database
+    my_cursor = my_db.cursor()
+
+    # Execute a SELECT query to fetch data
+    my_cursor.execute(
+        "SELECT * FROM states  WHERE name=%s ORDER BY id", (argv[4], ))
+
+    # fetch all the data returned by the query
+    my_data = my_cursor.fetchall()
+
+    # Iterate through the fetched data and print each row
+    for row in my_data:
+        print(row)
+
+    # Close all cursors
+    my_cursor.close()
+
+    # Close all databases
+    my_db.close()
